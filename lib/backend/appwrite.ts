3349272b5ca1098f554c49f488c50a37c0b17099
Motlakz <motlalepulasello5@gmail.com>
2025-04-07 14:17:58 +0200

    update: setup appwrite env

--- lib/backend/appwrite.ts
+++ lib/backend/appwrite.ts
@@ -1,18 +1,20 @@
-import { Client, Account, Databases } from 'appwrite';
+import { Account, Client, Databases } from 'appwrite';
 
+// Constants for collections and databases
+export const DATABASES = {
+    MAIN: process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
+} as const;
+
+export const COLLECTIONS = {
+    USERS: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION_ID!,
+    SUBSCRIPTIONS: process.env.NEXT_PUBLIC_APPWRITE_SUBSCRIPTIONS_COLLECTION_ID!,
+    PURCHASES: process.env.NEXT_PUBLIC_APPWRITE_PURCHASES_COLLECTION_ID!,
+} as const;
+
+// Initialize Appwrite client
 const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
 
-export const account = new Account(client);
 export const database = new Databases(client);
-
-export const COLLECTIONS = {
-    USERS: 'users',
-    PURCHASES: 'purchases',
-    SUBSCRIPTIONS: 'subscriptions',
-} as const;
-
-export const DATABASES = {
-    MAIN: process.env.APPWRITE_DATABASE_ID!,
-} as const;
+export const account = new Account(client);
